9ec5d9ffe5980349c898795c075056767e1c7b38
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-18 18:26:27 +0530

    read xpaths file, and update the paths

--- parser.py
+++ parser.py
@@ -1,27 +1,36 @@
 from pprint import pprint
 from lxml import html
 import re
-import time
+import json
+
+
+with open("xpaths.json", "r") as f:
+    XPATHS = json.load(f)
 
 def parser(text):
     tree = html.fromstring(text)
     result=[]
-
-    for outlets in tree.xpath('//div[@class="store-info-box"]'):
-        address = outlets.xpath('string(.//li[contains(@class,"outlet-address")])').strip()
+    city  = tree.xpath(f'string({XPATHS["OutletCity"]})').strip()
+    state = tree.xpath(f'string({XPATHS["OutletState"]})').strip()
+    store=tree.xpath(XPATHS["store_box"])
+    for outlets in store:
+        address = outlets.xpath(f'string({XPATHS["OutletAddress"]})').strip()
         pincode=re.search(r'\b(\d{6})\b', address)
-        landmark=outlets.xpath('string(.//li[not(@class)]//div[contains(@class,"info-text")])').strip() or "No Landmark available"
+        landmark=outlets.xpath(f'string({XPATHS["landmark"]})').strip() or "No Landmark available"
+        outletUrl=outlets.xpath(f'string({XPATHS["OutletUrl"]})').strip()
+        storeid=re.search(r'-(\d+)',outletUrl)
         outlets_dict={
-            "Outletname":outlets.xpath('string(.//li[contains(@class,"outlet-name")])').strip(),
+            "Outletname":outlets.xpath(f'string({XPATHS["Outletname"]})').strip(),
+            "storeId": int(storeid.group(1)),
             "OutletAddress":address,
             "pincode":pincode.group(1),
             "landmark":landmark,
-            "OutletCity":outlets.xpath('string(//*[@id="OutletCity"]/option[2])').strip(),
-            "OutletState" :outlets.xpath('string(//*[@id="OutletState"]/option[10])').strip(),
-            "Phone":outlets.xpath('string(.//li[contains(@class,"outlet-phone")])').strip(),
-            "timings":outlets.xpath('string(.//li[contains(@class,"outlet-timings")])').strip(),
-            "MapUrl":outlets.xpath('string(.//a[contains(@class,"map")]/@href)').strip(),
-            "OutletUrl":outlets.xpath('string(.//a[contains(@class,"website")]/@href)').strip()
+            "OutletCity":city,
+            "OutletState" :state,
+            "Phone":outlets.xpath(f'string({XPATHS["Phone"]})').strip(),
+            "timings":outlets.xpath(f'string({XPATHS["timings"]})').strip(),
+            "MapUrl": outlets.xpath(f'string({XPATHS["MapUrl"]})').strip(),
+            "OutletUrl":outletUrl
         }
         result.append(outlets_dict)
     return result
